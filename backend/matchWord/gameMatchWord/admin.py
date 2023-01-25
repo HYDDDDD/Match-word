e46c5dcfd07ff95c160d201cf90ae4b8d7252e15
Kantamas <pokkam55@gmail.com>
2023-01-26 01:13:28 +0700

    backend add User,Treasury,Score,Vocabulary

--- backend/matchWord/gameMatchWord/admin.py
+++ backend/matchWord/gameMatchWord/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User,Vocabulary
+from .models import User, Treasury, Score, Vocabulary
 
 admin.site.register(User)
+admin.site.register(Treasury)
+admin.site.register(Score)
 admin.site.register(Vocabulary)
